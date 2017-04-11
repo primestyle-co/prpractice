36cd205624286fbce621c93962cc4bc1ffcb0a15
suzuki ryosuke <suzuki@primestyle.co.jp>
2017-04-11 15:24:18 +0900

    fix php code

--- public/review.php
+++ public/review.php
@@ -1,18 +1,14 @@
 <?php
+namespace App;
 
-class Foo {
-  public function __construct($bar) {
-    $this->bar = $bar;
-  }
-  public function lorem() {
-    return true;
-  }
+class Foo
+{
+    public function __construct($bar)
+    {
+        $this->bar = $bar;
+    }
+    public function lorem()
+    {
+        return true;
+    }
 }
-
-$foo = new Foo();
-
-if($foo->lorem()) {
-  echo 'ipsam';
-}
-$foo->hoge();
-
